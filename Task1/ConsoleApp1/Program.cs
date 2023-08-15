bbf0a4888d9013ffe49ab6d9cd8540ad7c5a56fe
katyabarkalova <guliaev.p2014@yandex.ru>
2023-08-15 17:41:10 +0300

    task1

---
+++ Task1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using ConsoleApp1;
+class Program
+{
+    public static int SumMod(int[] numbers)
+    {
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 != 0)
+            {
+                count++;
+                if (count % 2 == 0)
+                    sum += numbers[i];
+            }
+        }
+        return Math.Abs(sum);
+    }
+    static void Main()
+    {
+        Console.WriteLine("Приложение, которое предоставит функционал сложения каждого второго нечетного числа из массива чисел тела запроса и вернёт их сумму по модулю.\n");
+        Console.WriteLine("Введите массив чисел через пробел:");
+        string[] numbersInput = Console.ReadLine().Split(' ');
+        int[] numbers = Array.ConvertAll(numbersInput, int.Parse);
+
+        // Проверка массива на наличие нечетных чисел
+        bool oddNumbersExist = false;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 != 0)
+            {
+                oddNumbersExist = true;
+                break;
+            }
+        }
+
+        if (!oddNumbersExist)
+        {
+            Console.WriteLine("В массиве нет нечетных чисел.");
+            return;
+        }
+
+        Console.WriteLine("1. Сумма каждого второго нечетного числа: " + SumMod(numbers));
+    }
+}
