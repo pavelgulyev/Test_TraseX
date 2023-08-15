776535c40e9fc31995ca05110bea1812425026d9
katyabarkalova <guliaev.p2014@yandex.ru>
2023-08-15 18:23:34 +0300

    Task2

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+Console.WriteLine("Введите строку:");
+string text = Console.ReadLine();
+
+Console.WriteLine(text);
+char[] obrtext = text.ToCharArray();
+Array.Reverse(obrtext);
+string finaltext = new string(obrtext);
+if (text == finaltext)
+{
+    Console.WriteLine("Данная строка является палиндромом");
+}
+if (text != finaltext)
+{
+    Console.WriteLine("Данная строка не является палиндромом");
+}
