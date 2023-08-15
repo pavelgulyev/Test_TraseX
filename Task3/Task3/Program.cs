46e64879817e97e821638f94e6e93e59e487e842
katyabarkalova <guliaev.p2014@yandex.ru>
2023-08-15 18:24:41 +0300

    Task 3

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+Console.WriteLine("Введите массив чисел через пробел:");
+string[] numbersInput = Console.ReadLine().Split(' ');
+
+List<int> numbers = new List<int>();
+numbers.AddRange(Array.ConvertAll(numbersInput, int.Parse));
+InsertionSort(numbers);
+
+
+Console.WriteLine("Отсортированный список:");
+foreach (int num in numbers)
+{
+    Console.Write(num + " ");
+}
+
+Console.ReadLine();
+
+// Метод сортировки вставками
+void InsertionSort(List<int> arr)
+{
+    int n = arr.Count;
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Перенос элементов списка, которые больше key, на одну позицию вперед
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = key;
+    }
+}
